f9e7bb1449bc83fd39759dcafdabec0be885b591
Nguyen Tien Dat <tie.dat2004@gmail.com>
2024-11-09 08:13:29 +0700

    abc

--- saleapp/index.py
+++ saleapp/index.py
@@ -1,20 +1,24 @@
-from itertools import product
 
 from flask import render_template, request
-from saleapp import app
 from unicodedata import category
+
+from saleapp import app
 
 import utils
 
 
 
-# Controller? Java
+# Controller
 
 @app.route("/")
 def home():
-    categories = utils.load_categories()
 
-    return render_template("client/index.html", categories=categories)
+    categories = utils.load_categories()
+    cate_id = request.args.get('category_id')
+    kw = request.args.get('kw')
+    prods = utils.load_products(kw=kw, cate_id=cate_id)
+
+    return render_template("client/homepage.html", categories=categories, products=prods)
 
 
 @app.route("/products")
@@ -27,11 +31,20 @@ def get_product_page():
     products = utils.load_products(cate_id, kw, from_price, to_price)
     return render_template("client/product.html", products=products)
 
+@app.route("/category/<int:category_id>")
+def get_category_page(category_id):
+    cate = Category.query.get_or_404(category_id)
+    products = utils.load_products(cate_id=category_id)
+
+    return render_template('product.html', products=products)
+
 
 @app.route("/product/<int:product_id>")
 def get_product_detail_page(product_id):
     product = utils.get_product_by_id(product_id)
-    return render_template("client/product-detail.html", product = product)
+    categories = utils.load_categories()
+    prods = utils.load_products()
+    return render_template("client/detail.html", product = product, categories=categories, products=prods)
 
 
 if __name__ == '__main__':
--- saleapp/utils.py
+++ saleapp/utils.py
@@ -1,36 +1,42 @@
-import json, os
-from email.errors import NonPrintableDefect
 
-from saleapp import app
+from saleapp.models import Category, Product
 
-# Service? Java
 
-def read_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
+# Service
+#
+# def read_json(path):
+#     with open(path, "r") as f:
+#         return json.load(f)
 
 def load_categories():
-    return read_json(os.path.join(app.root_path, 'data/categories.json'))
-
+    # return read_json(os.path.join(app.root_path, 'data/categories.json'))      -> get data from JSON
+    return Category.query.order_by('id').all()   # Get data from database
 
 def load_products(cate_id = None, kw = None, from_price = None, to_price = None):
-    products = read_json(os.path.join(app.root_path, 'data/product.json'))
+    # products = read_json(os.path.join(app.root_path, 'data/product.json'))
+    products = Product.query
+
     if cate_id:
-        products = [p for p in products if p['category_id']==int(cate_id)]
+        # products = [p for p in products if p['category_id']==int(cate_id)]
+        products = products.filter(Product.category_id == int(cate_id))
 
     if kw:
-        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+        # products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+        products =products.filter(Product.name.contains(kw))
 
     if from_price:
-        products = [p for p in products if p['price'] >= float(from_price)]
+        # products = [p for p in products if p['price'] >= float(from_price)]
+        products = products.filter(Product.price >= float(from_price))
 
     if to_price:
-        products = [p for p in products if p['price'] <= float(to_price)]
+        # products = [p for p in products if p['price'] <= float(to_price)]
+        products = products.filter(Product.price <= float(to_price))
 
-    return products
+    return products.all()
 
 def get_product_by_id(product_id):
-    products = read_json(os.path.join(app.root_path, 'data/product.json'))
-    for p in products:
-        if p['id']==product_id:
-            return p
+    # products = read_json(os.path.join(app.root_path, 'data/product.json'))
+    # for p in products:
+    #     if p['id']==product_id:
+    #         return
+    return Product.query.get(product_id)
